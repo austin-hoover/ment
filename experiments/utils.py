090148860534a2ad7cd7743894cfcb310bfa736c
austin-hoover <hooveram@ornl.gov>
2024-08-02 18:28:08 -0400

    Update experiments to include MENT-S tests with MCMC sampling

---
+++ experiments/utils.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import proplot as pplt
+import psdist as ps
+import psdist.plot as psv
+
+import ment
+
+
+def plot_corner_upper_lower(x_pred: np.ndarray, x_true: np.ndarray, n_bins: int, limits: list[tuple[float, float]]):
+    nsamp, ndim = x_pred.shape
+    
+    grid = psv.CornerGrid(ndim=ndim, figwidth=(ndim * 1.25), corner=False)
+    kws = dict(limits=limits, bins=n_bins, mask=True)
+    grid.plot_points(
+        x_true[:nsamp, :],
+        lower=False, 
+        diag_kws=dict(kind="step", color="red8", lw=1.25),
+        cmap=psv.cubehelix_cmap(color="red"),
+        **kws
+    )
+    grid.plot_points(
+        x_pred[:nsamp, :],
+        upper=False, 
+        diag_kws=dict(kind="step", color="blue8", lw=1.25), 
+        cmap=psv.cubehelix_cmap(color="blue"),
+        **kws
+    )
+    return grid.axs
