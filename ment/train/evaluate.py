935982d9f95a3941cb1986375c6fc5abbbeab2e1
austin-hoover <austin.michael.hoover@gmail.com>
2025-02-21 17:41:32 -0500

    Fix evaluator

--- ment/train/evaluate.py
+++ ment/train/evaluate.py
@@ -9,25 +9,20 @@ class Evaluator:
         self.n_samples = n_samples
 
     def __call__(self, model: MENT, x: np.ndarray = None) -> dict:
-        transforms = model.transforms
-        diagnostics = model.diagnostics
-        projections = model.projections
-
         if x is None:
             x = model.unnormalize(model.sample(self.n_samples))
+        x = x[:self.n_samples]
 
-        x = x[: self.n_samples, :]
-
-        simulate(x, transforms, diagnostics)
+        projections_pred = simulate(x, model.transforms, model.diagnostics)
+        projections_meas = model.projections
 
         discrepancy_vector = []
-        for hist_meas, hist_pred in zip(unravel(projections), unravel(diagnostics)):
+        for hist_meas, hist_pred in zip(unravel(projections_meas), unravel(projections_pred)):
             y_meas = hist_meas.values
-            y_pred = hist_pred.values
+            y_pred = hist_pred.values            
             discrepancy_vector.append(np.mean(np.abs(y_pred - y_meas)))
         discrepancy = sum(discrepancy_vector) / len(discrepancy_vector)
 
-        result = {
-            "mean_abs_error": discrepancy,
-        }
+        result = {}
+        result["mean_abs_error"] = discrepancy
         return result
