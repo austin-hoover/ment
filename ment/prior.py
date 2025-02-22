6d99efd775eee74f91248d9d5943159e8bbd79ae
austin-hoover <austin.michael.hoover@gmail.com>
2025-02-22 16:21:05 -0500

    Add UniformSphericalPrior

--- ment/prior.py
+++ ment/prior.py
@@ -1,41 +1,82 @@
 from typing import Callable
 import numpy as np
+import scipy.special
+
+
+def sphere_volume(ndim: int, radius: float) -> float:
+    factor = (np.pi ** (0.5 * ndim)) / scipy.special.gamma(1.0 + 0.5 * ndim)
+    return factor * (radius ** ndim)
 
 
 class Prior:
-    def __init__(self, **kws) -> None:
-        return
-
-    def _prob(self, x: np.ndarray) -> np.ndarray:
-        raise NotImplemented
+    def __init__(self, ndim: int, **kws) -> None:
+        self.ndim = ndim
 
     def prob(self, x: np.ndarray) -> np.ndarray:
-        return self._prob(x)
+        raise NotImplementedError
+
+    def sample(self, size: int) -> np.ndarray:
+        raise NotImplementedError
 
 
 class GaussianPrior(Prior):
-    def __init__(self, ndim: int, scale: np.ndarray, **kws) -> None:
+    def __init__(self, scale: np.ndarray, **kws) -> None:
         super().__init__(**kws)
-        self.ndim = ndim
         self.scale = scale
 
-    def _prob(self, x: np.ndarray) -> np.ndarray:
+    def prob(self, x: np.ndarray) -> np.ndarray:
         denom = np.sqrt((2.0 * np.pi) ** self.ndim) * np.sqrt(np.prod(self.scale))
-        _prob = np.exp(-0.5 * np.sum(np.square(x / self.scale), axis=1))
-        _prob = _prob / denom
-        return _prob
+        prob = np.exp(-0.5 * np.sum(np.square(x / self.scale), axis=1))
+        prob = prob / denom
+        return prob
 
-
+    def sample(self, size: int) -> np.ndarray:
+        x = np.random.normal(size=(size, self.ndim))
+        x = x * self.scale
+        return x
+    
+        
 class UniformPrior(Prior):
-    def __init__(self, ndim: int = 2, scale: float = 10.0, **kws) -> None:
+    def __init__(self, scale: float = 10.0, **kws) -> None:
         super().__init__(**kws)
         self.scale = scale
-        self.ndim = ndim
-        self.volume = scale**ndim
+        self.volume = scale**self.ndim
 
-    def _prob(self, x: np.ndarray) -> np.ndarray:
-        _prob = np.ones(x.shape[0]) / self.volume
+    def prob(self, x: np.ndarray) -> np.ndarray:
+        prob = np.ones(x.shape[0]) / self.volume
         for j in range(x.shape[1]):
-            _in_bounds = np.abs(x[:, j]) <= 0.5 * self.scale
-            _prob *= _in_bounds.astype(float)
-        return _prob
+            in_bounds = np.abs(x[:, j]) <= 0.5 * self.scale
+            prob *= in_bounds.astype(float)
+        return prob
+
+    def sample(self, size: int) -> np.ndarray:
+        x = np.random.uniform(-0.5, 0.5, size=(size, self.ndim))
+        x = x * self.scale
+        return x
+
+
+class UniformSphericalPrior(Prior):
+    def __init__(self, scale: float = 10.0, **kws) -> None:
+        super().__init__(**kws)
+        self.scale = scale
+        self.volume = sphere_volume(ndim=self.ndim, radius=self.scale)
+    
+    def prob(self, x: np.ndarray) -> np.ndarray:
+        prob = np.ones(x.shape[0]) / self.volume
+        r = np.sqrt(np.sum(np.square(x), axis=1))
+        prob[r > self.scale] = 0.0
+        return prob
+
+    def sample(self, size: int) -> np.ndarray:
+        # Sample from normal distribution
+        x = np.random.normal(size=(size, self.ndim))
+
+        # Scale to unit norm (KV distribution)
+        scale = 1.0 / np.sqrt(np.sum(x**2, axis=1))
+        x = x * scale[:, None]
+
+        # Fill sphere with uniform density
+        scale = np.random.uniform(0.0, 1.0, size=size) ** (1.0 / self.ndim)
+        scale = scale * self.scale
+        x = x * scale[:, None]
+        return x
