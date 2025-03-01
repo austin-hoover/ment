385d9def698e7a734c8b70d7d37a09ab9570814f
austin-hoover <austin.michael.hoover@gmail.com>
2025-03-01 13:46:44 -0500

    Add evaluate_model function
    This function is called by Evaluator for use in Trainer object,
    or it can be used on its own.

--- ment/train/evaluate.py
+++ ment/train/evaluate.py
@@ -4,25 +4,30 @@ from ..sim import simulate
 from ..utils import unravel
 
 
+def evaluate_model(model: MENT, nsamp: int) -> dict:
+    x = model.unnormalize(model.sample(nsamp))
+    projections_pred = unravel(simulate(x, model.transforms, model.diagnostics))
+    projections_meas = unravel(model.projections)
+    
+    discrepancy_vector = []
+    for projection_meas, projection_pred in zip(projections_meas, projections_pred):
+        y_meas = projection_meas.values
+        y_pred = projection_pred.values
+        discrepancy = np.mean(np.abs(y_pred - y_meas))
+        discrepancy_vector.append(discrepancy)
+
+    discrepancy_vector = np.array(discrepancy_vector)
+    discrepancy = np.sum(discrepancy_vector) / len(discrepancy_vector)
+
+    result = {}
+    result["discrepancy"] = discrepancy
+    return result
+
+
 class Evaluator:
-    def __init__(self, n_samples: int) -> None:
-        self.n_samples = n_samples
+    def __init__(self, nsamp: int) -> None:
+        self.nsamp = nsamp
 
-    def __call__(self, model: MENT, x: np.ndarray = None) -> dict:
-        if x is None:
-            x = model.unnormalize(model.sample(self.n_samples))
-        x = x[:self.n_samples]
+    def __call__(self, model: MENT) -> dict:
+        return evaluate_model(model, self.nsamp)
 
-        projections_pred = simulate(x, model.transforms, model.diagnostics)
-        projections_meas = model.projections
-
-        discrepancy_vector = []
-        for hist_meas, hist_pred in zip(unravel(projections_meas), unravel(projections_pred)):
-            y_meas = hist_meas.values
-            y_pred = hist_pred.values            
-            discrepancy_vector.append(np.mean(np.abs(y_pred - y_meas)))
-        discrepancy = sum(discrepancy_vector) / len(discrepancy_vector)
-
-        result = {}
-        result["mean_abs_error"] = discrepancy
-        return result
