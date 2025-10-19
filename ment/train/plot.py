b47b25a4dde3053c1d508ef6e72d59d30d21d221
austin-hoover <austin.michael.hoover@gmail.com>
2025-10-19 18:07:00 -0400

    Revamp plotting and remove ultraplot dependence

--- ment/train/plot.py
+++ ment/train/plot.py
@@ -1,12 +1,8 @@
 from typing import Callable
-from typing import TypeAlias
-from typing import Union
 
+import matplotlib.pyplot as plt
 import numpy as np
-import torch
-import psdist as ps
-import psdist.plot as psv
-import ultraplot as plt
+import scipy.ndimage
 
 from ..core import MENT
 from ..diag import Histogram
@@ -16,6 +12,135 @@ from ..sim import simulate
 from ..sim import simulate_with_diag_update
 from ..sim import copy_histograms
 from ..utils import unravel
+
+
+def plot_image(
+    values: np.ndarray,
+    edges: list[np.ndarray],
+    ax=None,
+    kind: str = "pcolor",
+    blur: float = 0.0,
+    **plot_kws,
+) -> None:
+    values = np.copy(np.array(values))
+    if blur:
+        values = scipy.ndimage.gaussian_filter(values, blur)
+
+    coords = [0.5 * (e[:-1] + e[1:]) for e in edges]
+    if kind == "pcolor":
+        ax.pcolormesh(coords[0], coords[1], values.T, **plot_kws)
+    elif kind == "contour":
+        ax.contour(coords[0], coords[1], values.T, **plot_kws)
+    elif kind == "contourf":
+        ax.contourf(coords[0], coords[1], values.T, **plot_kws)
+    else:
+        raise ValueError(f"Invalid kind {kind}")
+
+
+def plot_image_1d(
+    values: np.ndarray, edges: list[np.ndarray], ax=None, kind: str = "line", **plot_kws
+) -> None:
+    coords = 0.5 * (edges[:-1] + edges[1:])
+    if kind == "stairs":
+        plot_kws.pop("marker", None)
+        plot_kws.pop("ms", None)
+        ax.stairs(values, edges, **plot_kws)
+    elif kind == "line":
+        ax.plot(coords, values, **plot_kws)
+    else:
+        raise ValueError(f"Invalid kind {kind}")
+
+
+def plot_points(
+    x: np.ndarray, bins: int, limits: list[tuple[float, float]], ax=None, **plot_kws
+) -> None:
+    values, edges = np.histogramdd(x, bins=bins, range=limits)
+    return plot_image(values=values, edges=edges, ax=ax, **plot_kws)
+
+
+def plot_points_1d(
+    x: np.ndarray, bins: int, limits: tuple[float, float], ax=None, **plot_kws
+) -> None:
+    values, edges = np.histogram(x, bins=bins, range=limits)
+    return plot_image_1d(values=values, edges=edges, ax=ax, **plot_kws)
+
+
+class CornerGrid:
+    def __init__(self, ndim: int, **fig_kws) -> None:
+        self.ndim = ndim
+
+        self.fig_kws = fig_kws
+        self.fig_kws["ncols"] = ndim
+        self.fig_kws["nrows"] = ndim
+        self.fig_kws.setdefault("constrained_layout", True)
+
+        self.fig, self.axs = plt.subplots(**self.fig_kws)
+
+        for i in range(self.ndim):
+            for j in range(self.ndim):
+                if j > i:
+                    self.axs[i, j].axis("off")
+
+        for i in range(self.ndim):
+            for j in range(self.ndim):
+                if j > 0:
+                    self.axs[i, j].set_yticks([])
+                if i < self.ndim - 1:
+                    self.axs[i, j].set_xticks([])
+        self.axs[0, 0].set_yticks([])
+
+        for i in range(self.ndim):
+            self.axs[i, i].set_yticklabels([])
+
+        for ax in self.axs.flat:
+            for loc in ["top", "right"]:
+                ax.spines[loc].set_visible(False)
+
+    def plot(
+        self,
+        x: np.ndarray,
+        bins: int,
+        limits: list[tuple[float, float]],
+        diag_kws: dict = None,
+        **plot_kws,
+    ) -> None:
+        if diag_kws is None:
+            diag_kws = {}
+        diag_kws.setdefault("color", "black")
+        diag_kws.setdefault("lw", 1.25)
+        diag_kws.setdefault("kind", "stairs")
+
+        for i in range(self.ndim):
+            for j in range(i):
+                axis = (j, i)
+                plot_points(
+                    x[:, axis],
+                    bins=bins,
+                    limits=[limits[k] for k in axis],
+                    ax=self.axs[i, j],
+                    **plot_kws,
+                )
+
+        for i in range(self.ndim):
+            plot_points_1d(
+                x[:, i], bins=bins, limits=limits[i], ax=self.axs[i, i], **diag_kws
+            )
+
+        self.set_limits(limits)
+
+    def set_limits(self, limits: list[tuple[float, float]]) -> None:
+        for i in range(self.ndim):
+            for j in range(self.ndim):
+                if i != j:
+                    self.axs[i, j].set_ylim(limits[i])
+                self.axs[i, j].set_xlim(limits[j])
+
+    def set_labels(self, labels: list[str]) -> None:
+        for i in range(self.ndim):
+            self.axs[-1, i].set_xlabel(labels[i])
+            if i > 0:
+                self.axs[i, 0].set_ylabel(labels[i])
+        self.fig.align_ylabels()
 
 
 class Plotter:
@@ -71,11 +196,6 @@ class Plotter:
         projections_true = unravel(projections_true)
         projections_pred = unravel(projections_pred)
 
-        x_pred = x_pred.cpu().numpy()
-        for i in range(len(projections_true)):
-            projections_true[i] = projections_true[i].cpu().numpy()
-            projections_pred[i] = projections_pred[i].cpu().numpy()
-
         # Make plots
         figs = []
 
@@ -130,12 +250,14 @@ class PlotProj1D:
         self.plot_kws_pred = plot_kws_pred
         if self.plot_kws_pred is None:
             self.plot_kws_pred = {}
+
         self.plot_kws_pred.setdefault("color", "lightgray")
         self.plot_kws_pred.setdefault("lw", None)
 
         self.plot_kws_true = plot_kws_true
         if self.plot_kws_true is None:
             self.plot_kws_true = {}
+
         self.plot_kws_true.setdefault("color", "black")
         self.plot_kws_true.setdefault("lw", 0.0)
         self.plot_kws_true.setdefault("marker", ".")
@@ -148,6 +270,7 @@ class PlotProj1D:
         self.fig_kws = fig_kws
         if self.fig_kws is None:
             self.fig_kws = {}
+
         self.fig_kws.setdefault("sharex", False)
         self.fig_kws.setdefault("sharey", True)
 
@@ -162,14 +285,14 @@ class PlotProj1D:
 
         self.fig_kws.setdefault("ncols", ncols)
         self.fig_kws.setdefault("nrows", nrows)
-        self.fig_kws.setdefault("figsize", (1.50 * ncols, 1.25 * nrows))
+        self.fig_kws.setdefault("figsize", (1.50 * ncols, 1.1 * nrows))
 
         fig, axs = plt.subplots(**self.fig_kws)
 
         for i, (proj_pred, proj_true) in enumerate(
             zip(projections_pred, projections_true)
         ):
-            ax = axs[i]
+            ax = axs.flat[i]
 
             proj_pred = proj_pred.copy()
             proj_true = proj_true.copy()
@@ -178,17 +301,21 @@ class PlotProj1D:
             proj_pred.values /= scale
             proj_true.values /= scale
 
-            psv.plot_hist_1d(proj_pred, ax=ax, **self.plot_kws_pred)
-            psv.plot_hist_1d(proj_true, ax=ax, **self.plot_kws_true)
-            ax.format(ymin=self.ymin, ymax=self.ymax, xmin=self.xmin, xmax=self.xmax)
-            if self.log:
-                ax.format(yscale="log", yformatter="log")
+            plot_image_1d(
+                proj_pred.values, proj_pred.edges, ax=ax, **self.plot_kws_pred
+            )
+            plot_image_1d(
+                proj_true.values, proj_true.edges, ax=ax, **self.plot_kws_true
+            )
+            ax.set_ylim(self.ymin, self.ymax)
+            ax.set_xlim(self.xmin, self.xmax)
 
         if self.xlim_scale is not None:
             for ax in axs:
                 xlim = np.array(ax.get_xlim())
                 xlim = xlim * self.xlim_scale
-                ax.format(xlim=xlim)
+                ax.set_xlim(xlim)
+
         return (fig, axs)
 
 
@@ -219,12 +346,9 @@ class PlotProj2D_Contour:
         self.plot_kws.setdefault("kind", "contour")
         self.plot_kws.setdefault("levels", np.linspace(0.01, 1.0, 7))
         self.plot_kws.setdefault("lw", 0.75)
-        self.plot_kws.setdefault("process_kws", {})
-        self.plot_kws["process_kws"].setdefault("norm", "max")
-        self.plot_kws["process_kws"].setdefault("blur", 1.0)
 
-        self.plot_kws_true.setdefault("color", "black")
-        self.plot_kws_pred.setdefault("color", "red")
+        self.plot_kws_true.setdefault("colors", "black")
+        self.plot_kws_pred.setdefault("colors", "red")
 
         for key, val in self.plot_kws.items():
             self.plot_kws_true[key] = val
@@ -240,65 +364,59 @@ class PlotProj2D_Contour:
         nrows = int(np.ceil(nmeas / ncols))
 
         fig, axs = plt.subplots(
-            ncols=ncols, nrows=nrows, figwidth=(1.1 * ncols), share=self.lim_share
+            ncols=ncols,
+            nrows=nrows,
+            figsize=(1.1 * ncols, 1.1 * ncols),
+            sharex=self.lim_share,
+            sharey=self.limshare,
         )
         for index in range(nmeas):
             ax = axs[index]
             proj_true = projections_true[index]
             proj_pred = projections_pred[index]
-            psv.plot_hist(proj_true, ax=ax, **self.plot_kws_true)
-            psv.plot_hist(proj_true, ax=ax, **self.plot_kws_pred)
+            plot_image(proj_pred.values, proj_pred.edges, ax=ax, **self.plot_kws_pred)
+            plot_image(proj_true.values, proj_true.edges, ax=ax, **self.plot_kws_true)
 
         if self.lim_share:
-            ax = axs[0]
-            axs.format(xlim=np.multiply(ax.get_xlim(), self.lim_scale))
-            axs.format(ylim=np.multiply(ax.get_ylim(), self.lim_scale))
+            ax_ref = axs[0]
+            for ax in axs.flat:
+                ax.set_xlim(np.multiply(ax_ref.get_xlim(), self.lim_scale))
+                ax.set_ylim(np.multiply(ax_ref.get_ylim(), self.lim_scale))
         else:
-            for ax in axs:
+            for ax in axs.flat:
                 ax.format(xlim=np.multiply(ax.get_xlim(), self.lim_scale))
                 ax.format(ylim=np.multiply(ax.get_ylim(), self.lim_scale))
 
-        return fig, axs
+        return (fig, axs)
 
 
 class PlotDistCorner:
     def __init__(
         self,
-        log: bool = False,
+        bins: int,
+        limits: list[tuple[float, float]],
         fig_kws: dict = None,
         diag_ymin: float = None,
         diag_ymax: float = None,
-        **plot_kws,
+        plot_kws: dict = None,
     ) -> None:
-        self.log = log
 
         self.fig_kws = fig_kws
         if self.fig_kws is None:
             self.fig_kws = {}
 
         self.plot_kws = plot_kws
-        self.plot_kws.setdefault("bins", 64)
-        self.plot_kws.setdefault("mask", False)
+        if self.plot_kws is None:
+            self.plot_kws = {}
+
+        self.plot_kws.setdefault("bins", bins)
+        self.plot_kws.setdefault("limits", limits)
         self.plot_kws.setdefault("cmap", "viridis")
 
         self.diag_ymin = diag_ymin
         self.diag_ymax = diag_ymax
 
-        if self.log:
-            if self.diag_ymin is None:
-                self.diag_ymin = 1.00e-05
-            if self.diag_ymax is None:
-                self.diag_ymax = 5.0
-
-        if self.log:
-            self.plot_kws["norm"] = "log"
-
     def __call__(self, x: np.ndarray) -> tuple:
-        grid = psv.CornerGrid(x.shape[1], **self.fig_kws)
+        grid = CornerGrid(ndim=x.shape[1], **self.fig_kws)
         grid.plot(x, **self.plot_kws)
-
-        grid.format_diag(ymax=self.diag_ymax, ymin=self.diag_ymin)
-        if self.log:
-            grid.format_diag(yscale="log", yformatter="log")
-
         return (grid.fig, grid.axs)
