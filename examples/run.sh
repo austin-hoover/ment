d67cb1e797e1ef4e6533957cd451407b80878fba
austin-hoover <austin.michael.hoover@gmail.com>
2025-10-19 20:01:44 -0400

    Update .run.sh

--- examples/run.sh
+++ examples/run.sh
@@ -3,13 +3,19 @@
 find . -type d -name "*.ipynb_checkpoints" -exec rm -rf {} \;
 find . -type f -name "*.DS_Store" -exec rm -rf {} \;
 
-# find . -type f -name "*.py"    -exec echo {} \; -exec python {} \;
-# find . -type f -name "*.ipynb" -exec echo {} \; -exec jupyter nbconvert --inplace --ExecutePreprocessor.kernel_name=ment --execute {} \;
+
+for file in *.py ; do
+  if [ -f "$file" ]; then
+    echo "$file"
+    python $file
+  fi
+done
+
 
 for folder in ./*/; do
     cd $folder
     pwd
     find . -type f -name "*.py"    -exec echo {} \; -exec python {} \;
-    # find . -type f -name "*.ipynb" -exec echo {} \; -exec jupyter nbconvert --inplace --ExecutePreprocessor.kernel_name=ment --execute {} \;
+    find . -type f -name "*.ipynb" -exec echo {} \; -exec jupyter nbconvert --inplace --ExecutePreprocessor.kernel_name=ment --execute {} \;
     cd ..
 done
