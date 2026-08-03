8f3c09d08dfce78618aff53cdaf09df63d3b76cf
austin-hoover <austin.michael.hoover@gmail.com>
2026-08-03 15:25:35 -0400

    Separate covariance matrix fitting and analysis routines

---
+++ ment/cov/__init__.py
@@ -0,0 +1,4 @@
+from .cov import *
+from .fit import CovFitterBase
+from .fit import CholeskyCovFitter
+from .fit import LinearCovFitter
--- ment/cov/fit.py
+++ ment/cov/fit.py
@@ -1,4 +1,4 @@
-"""Covariance matrix analysis/fitting."""
+"""Covariance matrix fitting."""
 from collections.abc import Callable
 
 import numpy as np
@@ -7,125 +7,11 @@ import scipy.optimize
 from scipy.optimize import OptimizeResult
 from scipy.optimize import Bounds
 
-from .diag import Histogram
-from .utils import array_to_tensor
-
-
-def normalize_eigvec(v: torch.Tensor) -> torch.Tensor:
-    """Normalize eigenvectors according to Lebedev-Bogacz convention.
-
-    conj(v)^T U v = 2i
-    """
-    ndim = len(v)
-    v = torch.clone(torch.as_tensor(v))
-    U = build_poisson_matrix(ndim=ndim, complex=True).to(device=v.device)
-
-    def norm(vec: torch.Tensor) -> torch.Tensor:
-        return torch.linalg.multi_dot([torch.conj(v), U, v])
-
-    if torch.imag(norm(v)) > 0:
-        v = torch.conj(v)
-
-    v *= torch.sqrt(2.0 / torch.abs(norm(v)))
-    assert torch.isclose(torch.imag(norm(v)), torch.tensor(-2.0, device=v.device))
-    assert torch.isclose(torch.real(norm(v)), torch.tensor(0.0, device=v.device))
-    return v
-
-
-def build_poisson_matrix(ndim: int, complex: bool = False) -> torch.Tensor:
-    """Return 4 x 4 Poisson matrix (assumes x-x' ordering)."""
-    U = torch.zeros((ndim, ndim))
-    for i in range(0, ndim, 2):
-        U[i, i + 1] = +1.0
-        U[i + 1, i] = -1.0
-    if complex:
-        U = torch.complex(U, torch.zeros_like(U))
-    return U
-
-
-def build_norm_matrix_from_eigvecs(*eigvecs: list[torch.Tensor]) -> torch.Tensor:
-    """Return normalization matrix V^-1 from eigenvectors."""
-    ndim = eigvecs[0].shape[0]
-    V = torch.zeros((ndim, ndim))
-    for i, v in enumerate(eigvecs):
-        V[:, i * 2 + 0] = +v.real
-        V[:, i * 2 + 1] = -v.imag
-    return torch.linalg.inv(V)
-
-
-def build_scale_matrix(emittances: torch.Tensor) -> torch.Tensor:
-    """Return 4 x 4 emittance scaling matrix."""
-    diagonal = torch.clone(torch.as_tensor(emittances))
-    diagonal = torch.sqrt(torch.repeat_interleave(diagonal, 2))
-    return torch.diag(diagonal)
-
-
-def build_norm_matrix_from_cov(
-    cov_matrix: torch.Tensor, scale: bool = False
-) -> torch.Tensor:
-    """Return 4 x 4 symplectic normalization matrix from covariance matrix."""
-    S = cov_matrix
-    U = build_poisson_matrix(cov_matrix.shape[0])
-    U = U.to(S.device)
-    SU = torch.matmul(S, U)
-
-    eigvals, eigvecs = torch.linalg.eig(SU)
-
-    idx = eigvals.imag > 0.0
-    eigvecs = eigvecs[:, idx]
-
-    eigvecs = eigvecs.T
-    for i, v in enumerate(eigvecs):
-        eigvecs[i, :] = normalize_eigvec(v)
-
-    V_inv = build_norm_matrix_from_eigvecs(*eigvecs)
-    if scale:
-        A = torch.linalg.multi_dot([V_inv, S, V_inv.T])
-        A = torch.diag(torch.diag(A))
-        A = torch.sqrt(A)
-        A_inv = torch.linalg.inv(A)
-        V_inv = torch.matmul(A_inv, V_inv)
-    return V_inv
-
-
-def cov_to_corr(cov_matrix: torch.Tensor) -> torch.Tensor:
-    """Compute correlation matrix from covariance matrix."""
-    S = cov_matrix
-    D = torch.sqrt(torch.diag(torch.diag(S)))
-    Dinv = torch.linalg.inv(D)
-    return torch.linalg.multi_dot([Dinv, S, Dinv])
-
-
-def calc_rms_ellipse_params(cov_matrix: torch.Tensor) -> tuple[float, ...]:
-    """Return projected rms ellipse dimensions and orientation.
-
-    Args:
-        cov_matrix: Covariance matrix, shape (2, 2).
-
-    Returns
-        c1: Ellipse semi-axis #1.
-        c2: Ellipse semi-axis #2.
-        angle: Tilt angle below horizontal axis [rad].
-    """
-    sii = cov_matrix[0, 0]
-    sjj = cov_matrix[1, 1]
-    sij = cov_matrix[0, 1]
-
-    angle = -0.5 * torch.arctan2(2.0 * sij, sii - sjj)
-
-    _sin = torch.sin(angle)
-    _cos = torch.cos(angle)
-    _sin2 = _sin**2
-    _cos2 = _cos**2
-
-    c1 = torch.sqrt(abs(sii * _cos2 + sjj * _sin2 - 2 * sij * _sin * _cos))
-    c2 = torch.sqrt(abs(sii * _sin2 + sjj * _cos2 + 2 * sij * _sin * _cos))
-    return (c1, c2, angle)
+from ..diag import Histogram
+from ..utils import array_to_tensor
 
 
 class CovFitterBase:
-    """Base class for covariance matrix fitting."""
-
     def __init__(
         self,
         ndim: int,
@@ -136,7 +22,7 @@ class CovFitterBase:
         loss_scale: float = 1.0,
         emittance_penalty: float = 0.0,
     ) -> None:
-        """Constructor."""
+
         self.ndim = ndim
         self.nsamp = nsamp
         self.verbose = int(verbose)
@@ -162,15 +48,12 @@ class CovFitterBase:
         self.best_params = None
 
     def set_params(self, params: np.ndarray) -> None:
-        """Set covariance matrix parameters."""
         self.params = np.clip(params, self.lb, self.ub)
 
     def build_cov(self) -> torch.Tensor:
-        """Build covariance matrix from parameters."""
         raise NotImplementedError
 
     def sample(self, size: int = None) -> torch.Tensor:
-        """Sample particles from Gaussian distribution with current covariance matrix."""
         size = size or self.nsamp
         cov_matrix = self.build_cov().float()
         L = torch.linalg.cholesky(cov_matrix)
@@ -178,7 +61,6 @@ class CovFitterBase:
         return x @ L.T
 
     def loss_function(self, params: np.ndarray) -> float:
-        """Minimizes difference between predicted and measured moments."""
         self.set_params(params)
         x = self.sample()
 
@@ -213,8 +95,6 @@ class CovFitterBase:
     def fit(
         self, method: str = "differential_evolution", iters: int = 500, **opt_kws
     ) -> tuple[np.ndarray, OptimizeResult]:
-        """Fit parameters to data."""
-
         def callback_base():
             self.iteration += 1
             if self.verbose > 0:
@@ -224,8 +104,6 @@ class CovFitterBase:
             if self.verbose > 1 and self.ndim < 6:
                 print(f"cov_matrix:")
                 print(self.build_cov())
-
-        result = None
 
         if method == "simplex":
             opt_kws.setdefault("options", {})
@@ -369,10 +247,10 @@ class CholeskyCovFitter(CovFitterBase):
 class LinearCovFitter(CovFitterBase):
     """Parameterizes linear transformation of Gaussian base distribution.
 
-    There are N x N parameters with no bounds on the parameters.
+    There are N x N parameters with no bounds.
     """
 
-    def __init__(self, bound: float = 1.00e15, resample: bool = True, **kwargs) -> None:
+    def __init__(self, bound: float = 1.00e15, **kwargs) -> None:
         super().__init__(**kwargs)
         self.nparam = self.ndim**2
         self.ub = np.full(self.nparam, +bound)
